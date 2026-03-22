44aa1b0d1dfc4bd98b29f953ae0898be50c41ff6
Game-Dev-Shubham <shubhamyadav1247878@gmail.com>
2026-03-22 16:23:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
-const userData = [
-  {
-    name:"Shubham",
-    city:"Kushinagar",
-    age:20
-  },
-  
-  {
-    name:"Ajay",
-    city:"Kushinagar",
-    age:18
-  },
-  
-  {
-    name:"Aniket",
-    city:"Kushinagar",
-    age:13
-  },
-]
 const server = http.createServer((req,res)=>{
-   res.setHeader("Content-Type",'application/json');
-   
-   if(req.url=="login"){
-     res.write("Login Page");
-   }else if(req.url=="/"){
-     res.write(JSON.stringify(userData));
-   
-   }else{
-     res.write("Something Cool");
-   }
-   res.end();
-  
+  fs.readFile('web.html','utf8',(error,data)=>{
+    if(req.url == '/'){
+      if(error){
+        res.writeHead(404,{'Content-Type':'text/plain'});
+        res.end("Error");
+      }else if(data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+      }
+      
+    }else if(req.url == '/submit'){
+      res.end("submit");
+    }
+    
+    
+  });
 });
-server.listen(6000)
+server.listen(process.env.PORT);
