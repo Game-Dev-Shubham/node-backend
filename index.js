2ccd5b8ac6d24a4784addd6027465c792d98abea
Game-Dev-Shubham <shubhamyadav1247878@gmail.com>
2026-03-22 17:15:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const http = require('http');
 const fs = require('fs');
-const querryString = require('querystring');
+const querystring = require('querystring');
 
-const server = http.createServer((req,res)=>{
-  fs.readFile('web.html','utf8',(error,data)=>{
-    if(req.url == '/'){
-      if(error){
-        res.writeHead(404,{'Content-Type':'text/plain'});
+const server = http.createServer((req, res) => {
+
+  if (req.url == '/') {
+    fs.readFile('web.html', 'utf8', (error, data) => {
+      if (error) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end("Error");
-      }else if(data){
-        res.writeHead(200,{'Content-Type':'text/html'});
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(data);
       }
-      
-    }else if(req.url == '/submit'){
-      
-      let usersData = [];
-      req.on('data',(chunk)=>{
-        usersData.push(chunk);
-      });
-     
-      req.on('end',()=>{
-        let body = Buffer.concat(usersData).toString();
-        let prasedData = querryString.parse(body);
-        res.end(JSON.stringify(prasedData));
-      });
-     
-      
-    }
-    
-    
-  });
+    });
+  }
+
+  else if (req.url == '/submit' && req.method == 'POST') {
+
+    let usersData = [];
+
+    req.on('data', (chunk) => {
+      usersData.push(chunk);
+    });
+
+    req.on('end', () => {
+      let body = Buffer.concat(usersData).toString();
+      let parsedData = querystring.parse(body);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(parsedData));
+    });
+
+  }
+
 });
-server.listen(process.env.PORT);
+
+server.listen(process.env.PORT || 3000);
