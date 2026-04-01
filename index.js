d045352b531f2db7f5169ba24181deec46e3c1f2
Game-Dev-Shubham <shubhamyadav1247878@gmail.com>
2026-04-01 18:51:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
 const http = require('http');
 const fs = require('fs');
 const querystring = require('querystring');
+const { MongoClient } = require('mongodb');
 
-const server = http.createServer((req, res) => {
+// 🔗 Connection URL
+const uri = "mongodb+srv://gamedev:shubham@first-cluster.n8jtsrp.mongodb.net/gameDB";
 
-  if (req.url == '/') {
-    fs.readFile('web.html', 'utf8', (error, data) => {
-      if (error) {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end("Error");
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  }
+const client = new MongoClient(uri);
 
-  else if (req.url == '/submit' && req.method == 'POST') {
+async function startServer() {
+  await client.connect();
+  console.log("MongoDB Connected 🔥");
 
-    let usersData = [];
+  const db = client.db("gameDB");
+  const usersCollection = db.collection("users");
 
+  const server = http.createServer((req, res) => {
 
-    req.on('data', (chunk) => {
-      usersData.push(chunk);
-    });
+    if (req.url == '/') {
+      fs.readFile('web.html', 'utf8', (error, data) => {
+        if (error) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end("Error");
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    }
 
-    req.on('end', () => {
-      let body = Buffer.concat(usersData).toString();
-      let parsedData = querystring.parse(body);
+    else if (req.url == '/submit' && req.method == 'POST') {
 
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(parsedData));
-    });
+      let usersData = [];
 
-  }
+      req.on('data', (chunk) => {
+        usersData.push(chunk);
+      });
 
-});
+      req.on('end', async () => {
+        let body = Buffer.concat(usersData).toString();
+        let parsedData = querystring.parse(body);
 
-server.listen(process.env.PORT || 3000);
+        // 🔥 MongoDB me save
+        await usersCollection.insertOne(parsedData);
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: "Data saved", data: parsedData }));
+      });
+
+    }
+
+  });
+
+  server.listen(process.env.PORT || 3000, () => {
+    console.log("Server running 🚀");
+  });
+}
+
+startServer();
